8d74c9ab74335089007730b9fe41ae8bcdef1fcd
novy <45167857+novysodope@users.noreply.github.com>
2024-01-22 18:06:37 +0800

    Update ScanAPKPermis.java

--- ScanAPKPermis.java
+++ ScanAPKPermis.java
@@ -8,13 +8,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class APKTools {
+    public static void fingerprint(){
+        System.out.println("\n" +
+                "   _____                      _ _                         _____                    __                      _____  _____  \n" +
+                "  / ____|                    | (_)                       / ____|                  / _|               /\\   |  __ \\|  __ \\ \n" +
+                " | |     ___  _ __ ___  _ __ | |_  __ _ _ __   ___ ___  | (___   ___ __ _ _ __   | |_ ___  _ __     /  \\  | |__) | |__) |\n" +
+                " | |    / _ \\| '_ ` _ \\| '_ \\| | |/ _` | '_ \\ / __/ _ \\  \\___ \\ / __/ _` | '_ \\  |  _/ _ \\| '__|   / /\\ \\ |  ___/|  ___/ \n" +
+                " | |___| (_) | | | | | | |_) | | | (_| | | | | (_|  __/  ____) | (_| (_| | | | | | || (_) | |     / ____ \\| |    | |     \n" +
+                "  \\_____\\___/|_| |_| |_| .__/|_|_|\\__,_|_| |_|\\___\\___| |_____/ \\___\\__,_|_| |_| |_| \\___/|_|    /_/    \\_\\_|    |_|     \n" +
+                "                       | |                                                                                               \n" +
+                "                       |_|                                                                                               \n" +
+                "This version(0.1) is only used to extract all permission attributes of APK. Author: novy, Github: https://github.com/novysodope/LittleTools\n\n");
+    }
+
     public static void main(String[] args) {
+        fingerprint();
         if (args.length < 0 || args.length == 0) {
             System.out.println("Please Enter \"-h\" See Help");
             System.exit(0);
         }
         String apkPath = args[0];
-
         if (apkPath.equals("-h")) {
             System.out.println("java -jar ScanPermis.jar example.apk  --Single");
             System.out.println("java -jar ScanPermis.jar -f /folder_example/  --Batch\n");
@@ -23,15 +36,24 @@ public class APKTools {
 
         if (apkPath.equals("-f")) {
             String batch = args[1];
+            File file = new File(batch);
+            if (file.isFile()){
+                System.out.println("You entered is not a folder");
+                System.exit(0);
+            }
             analyzeApkFolder(batch);
             System.exit(0);
         }
-
-        System.out.println("File from extraction " + apkPath);
-        String fileName = extractFileName(apkPath);
-        System.out.println("Folder: " + fileName);
-        List<String> permissions = scanApkPermissions(apkPath, fileName);
-        System.out.println(formatTable(permissions));
+        
+        File file = new File(apkPath);
+        if (file.isFile()) {
+            System.out.println("File from extraction " + apkPath);
+            String fileName = extractFileName(apkPath);
+            System.out.println("Folder: " + fileName);
+            List<String> permissions = scanApkPermissions(apkPath, fileName);
+            System.out.println(formatTable(permissions));
+        }
+        System.out.println("Please check your input!");
     }
 
     // apktool会根据文件名创建文件夹，这里提取文件名方便给scanApkPermissions方法用
